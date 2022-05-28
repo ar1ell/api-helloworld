cd8a0b0b07a9851c78e993dbd32fac397fc416f6
Ariel Agramonte <ariel.nt@hotmail.com>
2022-05-27 21:20:36 -0300

    comandos sql inseridos

--- routes/produtos.js
+++ routes/produtos.js
@@ -1,47 +1,100 @@
 const express = require('express');
 const router = express.Router();
+const mysql = require('../mysql').pool;
 
 router.get('/', (req, res, next) => {
-    res.status(200).send({
-        mensagem: 'Usando o GET dentro de produtos'
+
+    mysql.getConnection((error, conn) => {
+        if (error) {return res.status(500).send({error: error})};
+        conn.query(
+            'SELECT * FROM produtos',
+            (error, resultado, fields) => {
+                if (error) {
+                    return res.status(500).send({
+                        error: error,
+                        response: null
+                    });
+                }
+                return res.status(200).send({response: resultado});       
+            }
+        );
     });
 });
 
 router.post('/', (req, res, next) => {
-    
-    const produto = {
-        nome: req.body.nome,
-        preco: req.body.preco
-    };
 
-    res.status(201).send({
-        mensagem: 'Insere um produto',
-        produtoCriado: produto 
+    mysql.getConnection((error, conn) => {
+        if (error) {return res.status(500).send({error: error})};
+        conn.query(
+            'INSERT INTO produtos (nome, preco) VALUES (?,?)',
+            [req.body.nome, req.body.preco],
+            (error, resultado, field) => {
+                conn.release();
+                if (error) {
+                    return res.status(500).send({
+                        error: error,
+                        response: null
+                    });
+                }
+                res.status(201).send({
+                    mensagem: 'Produto inserido com sucesso',
+                    id_produto: resultado.insertId 
+                });    
+            }
+        )
     });
+
 });
 
 // Retorna os dados de um produto
 
 router.get('/:id_produto', (req, res, next) => {
-    const id = req.params.id_produto
-
-    if (id == 'especial'){
-        res.status(200).send({
-            mensagem: 'Usando o GET de um produto exclusivo',
-            id: id
-        });
-    } else {
-        res.status(200).send({
-            mensagem: 'Voce passou um ID'
-        });
-    }
+    mysql.getConnection((error, conn) => {
+        if (error) {return res.status(500).send({error: error})};
+        conn.query(
+            'SELECT * FROM produtos WHERE id_produto = ?;',
+            [req.params.id_produto],
+            (error, resultado, fields) => {
+                if (error) {
+                    return res.status(500).send({
+                        error: error,
+                        response: null
+                    });
+                }
+                return res.status(200).send({response: resultado});       
+            }
+        );
+    });
 });
 
 
 // Altera um produto
 router.patch('/', (req, res, next) => {
-    res.status(200).send({
-        mensagem: 'Usando o PATCH dentro de produtos'
+    mysql.getConnection((error, conn) => {
+        if (error) {return res.status(500).send({error: error})};
+        conn.query(
+            `UPDATE
+                produtos
+            SET
+                nome = ?,
+                preco = ?
+            WHERE
+                id_produto = ?`,
+            [req.body.nome, req.body.preco, req.body.id_produto],
+            (error, resultado, field) => {
+                conn.release();
+                if (error) {
+                    return res.status(500).send({
+                        error: error,
+                        response: null
+                    });
+                }
+                res.status(201).send({
+                    mensagem: 'Produto alterado com sucesso',
+                    id_produto: resultado.insertId 
+                });    
+            }
+        )
     });
 });
 
